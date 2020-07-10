14e8c53768c9f15ff989ed54dccf9e2d6451ef0d
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-07-10 17:35:46 +0530

    _s framework with some changes

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        frontend: ['./js/main.js', './sass/style.scss']
+        // customizer: './js/customizer.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'assets/'),
+        filename: './js/main.js'
+    },
+    mode: 'development',
+    devtool: 'cheap-eval-source-map',
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                exclude: /node_modules/,
+                test: /\.jsx$/,
+                loader: 'eslint-loader'
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            // {
+            //     test: /\.s?css$/,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            // },
+
+            // compile all .scss files to plain old css after adding pre-fixer
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    'sass-loader']
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-sprite-loader',
+                options: {
+                    extract: true,
+                    spriteFilename: 'svg-defs.svg'
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif)\$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images/',
+                            name: '[name].[ext]'
+                        }
+                    },
+                    'img-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new StyleLintPlugin(),
+        new MiniCssExtractPlugin({
+            // path: path.resolve(__dirname, 'assets/css/'),
+            filename: '../style.css'
+        }),
+        new BrowserSyncPlugin({
+            files: '**/*.php',
+            proxy: 'http://localhost:10009/'
+        })
+    ],
+    optimization: {
+        minimizer: [new UglifyJsPlugin(), new OptimizeCssAssetsPlugin()]
+    }
+};
