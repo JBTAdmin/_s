144dbd5cac9952038d2e13a298ce7ecf77b12d99
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-08-02 07:40:14 +0530

    Before Formatting all php files

--- page.php
+++ page.php
@@ -20,31 +20,30 @@ if ( is_active_sidebar( 'sidebar-1' ) ) {
 }
 
 ?>
-    <div class="site-container">
-        <div class="wrap">
-            <div class="main-container">
-                <main id="primary" class="site-main primary-content <?php echo esc_attr( $sidebar_page_class ); ?>">
-					
+	<div class="site-container">
+		<div class="wrap">
+			<div class="main-container">
+				<main id="primary" class="site-main primary-content <?php echo esc_attr( $sidebar_page_class ); ?>">
 					<?php
 					while ( have_posts() ) :
 						the_post();
-						
+
 						get_template_part( 'template-parts/content', 'page' );
-						
+
 						// If comments are open or we have at least one comment, load up the comment template.
 						if ( comments_open() || get_comments_number() ) :
 							comments_template();
 						endif;
-					
+
 					endwhile; // End of the loop.
 					?>
 
-                </main><!-- #main -->
+				</main><!-- #main -->
 				<?php
 				get_sidebar();
 				?>
-            </div>
-        </div>
-    </div> <!-- #container -->
+			</div>
+		</div>
+	</div> <!-- #container -->
 <?php
 get_footer();
