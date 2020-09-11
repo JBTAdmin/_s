973456731a94e1e09d4d6cbc2e8c6bc48948d587
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-09-11 07:50:41 +0530

    Some fixes for Menu icon on sidebar

--- js/main.js
+++ js/main.js
@@ -1,57 +1,78 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const menuBtn = document.querySelector(".menu-btn");
-  const menu = document.querySelector(".menu");
-  const header = document.querySelector("header");
-  const hamburgerBtn = document.querySelector(".sidebar-open");
-  const aside = document.querySelector("aside");
-  const menuCloseBtn = document.querySelector(".sidebar-close");
-  const sidebarOverlay = document.querySelector(".sidebar-overlay");
+/**
+ * File main.js.
+ *
+ * Contains events and methods to handle menus and sidebars.
+ *
+ * @package aaurora
+ */
 
-  const searchField = document.querySelector(".search-field");
+document.addEventListener(
+	"DOMContentLoaded",
+	function(event) {
+		const menuBtn        = document.querySelector( ".menu-btn" );
+		const menu           = document.querySelector( ".menu" );
+		const header         = document.querySelector( "header" );
+		const hamburgerBtn   = document.querySelector( ".sidebar-open" );
+		const aside          = document.querySelector( "aside" );
+		const menuCloseBtn   = document.querySelector( ".sidebar-close" );
+		const sidebarOverlay = document.querySelector( ".sidebar-overlay" );
 
-  const sidebarMenu = Array.prototype.slice.call(document.querySelectorAll(
-      ".widget-area .menu-item-has-children"
-  ));
+		const searchField = document.querySelector( ".search-field" );
 
-  const sidebarMenuLink = Array.prototype.slice.call(document.querySelectorAll(
-      ".widget-area .menu-item-has-children > a"
-  ));
+		const subMenu = document.querySelectorAll( ".sidebar-menu  .sub-menu" );
 
-  let menuBtClass = false;
+		subMenu.forEach(
+			el =>
+			el.insertAdjacentHTML( 'beforebegin', '<span class="sub-menu-btn-icon">&gt;</span>' )
+		);
 
-  sidebarMenuLink.forEach(function(el) {
-    el.addEventListener("click", function(event) {
-      event.preventDefault();
-    })
-  });
+		const sidebarMenu = document.querySelectorAll( ".widget-area .sub-menu-btn-icon" );
 
-  sidebarMenu.forEach(function(el) {
-    el.addEventListener("click", function(event) {
-      this.querySelector(".sub-menu").classList.toggle("display-block");
-      el.classList.toggle("active");
-    })
-  });
+		sidebarMenu.forEach(
+			function(el) {
+				el.addEventListener(
+					"click",
+					function(event) {
+						this.parentNode.querySelector( ".sub-menu" ).classList.toggle( "display-block" );
+						el.classList.toggle( "active" );
+					}
+				)
+			}
+		);
 
-  menuBtn.addEventListener("click", function() {
-    menuBtn.classList.toggle("open");
-    menu.classList.toggle("open");
-    header.classList.toggle("open");
-  });
+		menuBtn.addEventListener(
+			"click",
+			function() {
+				menuBtn.classList.toggle( "open" );
+				menu.classList.toggle( "open" );
+				header.classList.toggle( "open" );
+			}
+		);
 
-  hamburgerBtn.addEventListener("click", function() {
-    searchField.focus();
-    aside.classList.toggle("open");
-    sidebarOverlay.classList.toggle("open");
-  });
+		hamburgerBtn.addEventListener(
+			"click",
+			function() {
+				searchField.focus();
+				aside.classList.toggle( "open" );
+				sidebarOverlay.classList.toggle( "open" );
+			}
+		);
 
-  menuCloseBtn.addEventListener("click", function() {
-    hamburgerBtn.focus();
-    aside.classList.toggle("open");
-    sidebarOverlay.classList.toggle("open");
-  });
+		menuCloseBtn.addEventListener(
+			"click",
+			function() {
+				hamburgerBtn.focus();
+				aside.classList.toggle( "open" );
+				sidebarOverlay.classList.toggle( "open" );
+			}
+		);
 
-  sidebarOverlay.addEventListener("click", function() {
-    aside.classList.toggle("open");
-    sidebarOverlay.classList.toggle("open");
-  });
-});
+		sidebarOverlay.addEventListener(
+			"click",
+			function() {
+				aside.classList.toggle( "open" );
+				sidebarOverlay.classList.toggle( "open" );
+			}
+		);
+	}
+);
