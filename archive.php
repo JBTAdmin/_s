349c93e26084d7196b5d3dc93142d159376e9bd4
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-09-23 22:13:17 +0530

    New Blog Layout

--- archive.php
+++ archive.php
@@ -43,7 +43,8 @@ $sidebar_page_class = ' sidebar_position_' . get_theme_mod( 'sidebar_archive', '
 								 * If you want to override this in a child theme, then include a file
 								 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 								 */
-								get_template_part( 'template-parts/blog/blog-layout-2', get_post_type() );
+								//todo probably name can be used here instead of concatenation
+								get_template_part( 'template-parts/blog/blog-' . get_theme_mod( 'blog_layout_setting', 'layout-3' ), get_post_type() );
 
 							endwhile;
 							?>
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,23 +9,30 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post-article' ); ?>>
-	<div  class="blog-type-list">
-	<?php aaurora_post_thumbnail( 'aaurora-blog-1-featured-image', aaurora_posted_on( true ) ); ?>
-	<div class="entry-header">
-		<?php if ( 'post' === get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php
-				aaurora_posted_on();
-				aaurora_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post-article-3' ); ?>>
 
-	</div><!-- .entry-header -->
-	<a href="<?php echo esc_url( get_permalink() ); ?>" class="btn btn-link brand-color-hover">
-		<?php esc_html_e( 'Read More', 'aaurora' ); ?>
-	</a>
-	</div>
+    <div  class="blog-type-list" style="background-image:url('<?php echo get_the_post_thumbnail_url(  get_the_ID(),'full' ); ?>')">
+    </div>
+    <a href=" <?php echo esc_url( get_permalink() ) ?>" rel="bookmark"></a>
+    <div class="blog-type-list-content">
+        <!--            todo class names proper-->
+        <div class="post-date">
+            <span class="post-date-day"><?php echo get_the_date( 'd' ) ?></span>
+            <span class="post-date-month"><?php echo get_the_date( 'F' ) ?></span>
+        </div>
+        <div class="entry-header">
+			<?php
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			?>
+            <div class="blog-list-content-meta-info">
+				<?php
+				aaurora_meta_category_list();
+				?>
+            </div><?php
+			//				load_inline_svg('right-arrow.svg');
+			?>
+        </div><!-- .entry-header -->
+    </div>
+
+
 </article><!-- #post-<?php the_ID(); ?> -->
