15b3ac3ca4821b84eec6b38de6e1ad5a7722ab02
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-11-20 08:26:37 +0530

    New Blog Layout Added

--- template-parts/blog/blog-layout-2.php
+++ template-parts/blog/blog-layout-2.php
@@ -12,24 +12,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post-article-2' ); ?>>
 	<div  class="blog-type-list">
 		<div class="blog-article">
-<!--			--><?php //aaurora_post_thumbnail( 'aaurora-blog-2-featured-image', aaurora_posted_on( true ) ); ?>
+			<?php aaurora_post_thumbnail( 'aaurora-blog-2-featured-image', aaurora_posted_on( true ) ); ?>
 			<div class="blog-type-list-content">
-                <div class="blog-type-published-date">
-                    <?php echo aaurora_posted_on( true ) ?>
-                </div>
 				<div class="entry-header">
 					<?php
 					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 					?>
 				</div><!-- .entry-header -->
-                <div class="blog-type-post-category">
-                    <?php
-                        $categories = get_the_category();
-                        if ( ! empty( $categories ) ) {
-                            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-                        }
-                    ?>
-                </div>
 			</div>
 		</div>
 	</div>
---
+++ template-parts/blog/blog-layout-4.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aaurora
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'blog-post-article-4' ); ?>>
+	<div  class="blog-type-list">
+		<div class="blog-article">
+<!--			--><?php //aaurora_post_thumbnail( 'aaurora-blog-2-featured-image', aaurora_posted_on( true ) ); ?>
+			<div class="blog-type-list-content">
+                <div class="blog-type-published-date">
+                    <?php echo aaurora_posted_on( true ) ?>
+                </div>
+				<div class="entry-header">
+					<?php
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					?>
+				</div><!-- .entry-header -->
+                <div class="blog-type-post-category">
+                    <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                        }
+                    ?>
+                </div>
+			</div>
+		</div>
+	</div>
+
+</article><!-- #post-<?php the_ID(); ?> -->
