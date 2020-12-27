eb381707110a8f18bbdca96a7bbe265b362e1be8
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-12-28 01:00:37 +0530

    blog 2 DONE

--- template-parts/blog/blog-2.php
+++ template-parts/blog/blog-2.php
@@ -11,17 +11,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>">
-	<div  class="blog-type-list">
-		<div class="blog-article">
+    <div class="inner-entry">
+        <div class="featured-image">
 			<?php aaurora_post_thumbnail( 'aaurora-blog-2-featured-image', aaurora_posted_on( true ) ); ?>
-			<div class="blog-type-list-content">
-				<div class="entry-header">
-					<?php
-					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-					?>
-				</div><!-- .entry-header -->
-			</div>
-		</div>
-	</div>
+        </div>
+        <div class="entry-header">
+			<?php
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			?>
+        </div><!-- .entry-header -->
+    </div>
 
 </article><!-- #post-<?php the_ID(); ?> -->
