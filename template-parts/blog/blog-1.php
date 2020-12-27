e1fd033873550adbdf4fda74510c0ffec320d1b9
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-12-28 00:51:00 +0530

    Blog 1 renaming completed

--- template-parts/blog/blog-1.php
+++ template-parts/blog/blog-1.php
@@ -11,28 +11,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>">
-	<div  class="blog-type-list">
-		<div class="article-featured-image" >
+	<div class="inner-entry">
+		<div class="featured-image" >
 			<?php aaurora_post_thumbnail( 'full', aaurora_posted_on( true ) ); ?>
 		</div>
 
-		<div class="blog-article-metainfo">
-			<div class="post-date">
+		<div class="entry-header">
+			<div class="posted-on">
 				<?php aaurora_posted_on(); ?>
 			</div>
-			<div class="article-header">
+			<div class="entry-title">
 				<?php
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 				?>
 			</div>
 		</div>
-		<div class="blog-article-content">
+		<div class="entry-summary">
 			<?php aaurora_excerpt( 20 ); ?>
+            <div class="read-more">
+                <a href="<?php echo esc_url( get_permalink() ); ?>"><?php esc_html_e( 'read more', 'aaurora' ); ?> <?php load_inline_svg( 'arrow-right-circle.svg' ); ?></a>
+            </div>
 		</div>
-		<div class="read-more">
-			<a href="<?php echo esc_url( get_permalink() ); ?>"><?php esc_html_e( 'read more', 'aaurora' ); ?> <?php load_inline_svg( 'arrow-right-circle.svg' ); ?></a>
-		</div>
-
 	</div>
-
 </article><!-- #post-<?php the_ID(); ?> -->
