bc02ded1e7123cb90f54f1f8d15a65066898afe0
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2020-12-28 12:40:41 +0530

    Code formatting

--- template-parts/blog/blog-4.php
+++ template-parts/blog/blog-4.php
@@ -25,14 +25,14 @@
             </div><!-- .entry-header -->
             <div class="entry-footer">
                 <div class="entry-meta">
-                <span class="cat-links">
-                    <?php
-                    $categories = get_the_category();
-                    if ( ! empty( $categories ) ) {
-	                    echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-                    }
-                    ?>
-                </span>
+                    <span class="cat-links">
+                        <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                        }
+                        ?>
+                    </span>
                 </div>
             </div>
         </div>
