c25d7e592a95f59d6ab21d80936ecbaa9331e9e1
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2021-04-16 20:14:42 +0530

    Aligned Search form in different places.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -679,18 +679,7 @@ function gautam_footer_popup_search_modal_layout() {
 		<a href="#" class="popup_modal_close_button">
 			<i class="fa fa-times fa-lg" aria-hidden="true"></i>
 		</a>
-		<div class="search_holder">
-			<form role="search" class="search search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>"
-					method="GET">
-					<label> <span class="screen-reader-text">Search for</span>
-						<input autocomplete="off" type="text" id="search-field" class="search-field" name="s"
-								placeholder="<?php esc_attr_e( 'Search..', 'gautam' ); ?>" value="" autofocus>
-						<button type="submit" class="search search-submit">
-							<i class="fa fa-search" aria-hidden="true"></i>
-						</button>
-					</label>
-			</form>
-		</div>
+		<?php get_search_form() ?>
 	</div>
 	<?php
 }
--- searchform.php
+++ searchform.php
@@ -9,13 +9,16 @@
 
 ?>
 
-<form role="search" class="search search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>"
-	method="GET">
-	<label> <span class="screen-reader-text">Search for</span>
-		<input autocomplete="off" type="text" id="search-field" class="search-field" name="s"
-				placeholder="<?php esc_attr_e( 'Search..', 'gautam' ); ?>" value="" autofocus>
-		<button type="submit" class="search search-submit">
-			<i class="fa fa-search" aria-hidden="true"></i>
-		</button>
-	</label>
-</form>
+<!--search form dispalayed whereever get_search_form() is called.-->
+<div class="search_holder">
+    <form role="search" class="search search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>"
+          method="GET">
+        <label> <span class="screen-reader-text">Search for</span>
+            <input autocomplete="off" type="text" id="search-field" class="search-field" name="s"
+                   placeholder="<?php esc_attr_e( 'Search..', 'gautam' ); ?>" value="" >
+            <button type="submit" class="search search-submit">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </button>
+        </label>
+    </form>
+</div>
